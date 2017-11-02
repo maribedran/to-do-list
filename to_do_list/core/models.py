11f18556c745ca6e6aa83ce4f047da2c37330735
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-02 16:43:51 -0200

    Create Task model

--- to_do_list/core/models.py
+++ to_do_list/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext as _
+from django.utils.text import Truncator
 
 
 class ToDoList(models.Model):
@@ -12,4 +13,18 @@ class ToDoList(models.Model):
 
     class Meta:
         verbose_name = _('To-Do List')
+
+
+class Task(models.Model):
+    description = models.CharField(_('Description'), max_length=255)
+    to_do_list = models.ForeignKey(verbose_name=_('To-Do List'), to=ToDoList, related_name='tasks')
+    created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
+    due_at = models.DateTimeField(_('Due at'), null=True, blank=True)
+    done_at = models.DateTimeField(_('Done at'), null=True, blank=True)
+
+    def __str__(self, *args, **kwargs):
+        return Truncator(self.description).chars(70)
+
+    class Meta:
+        verbose_name = _('Task')
 
--- to_do_list/core/tests/test_models.py
+++ to_do_list/core/tests/test_models.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.utils import timezone
 from freezegun import freeze_time
+from model_mommy import mommy
 
-from core.models import ToDoList
+from core.models import ToDoList, Task
 
 
 class ToDoListModelTest(TestCase):
@@ -22,4 +23,38 @@ class ToDoListModelTest(TestCase):
         self.assertTrue(saved_instance.id)
         self.assertEqual('Shopping List', saved_instance.name)
         self.assertEqual(now, saved_instance.created_at)
+
+
+class TaskModelTest(TestCase):
+
+    @freeze_time(timezone.now())
+    def test_save_persists_all_fields(self):
+        now = timezone.now()
+        objects_count = Task.objects.count()
+
+        to_do_list = mommy.make(ToDoList)
+        instance = Task(
+            description='Buy beer',
+            to_do_list=to_do_list,
+            due_at=now,
+            done_at=now
+        )
+
+        self.assertIsNone(instance.created_at)
+
+        instance.save()
+
+        self.assertEqual(objects_count + 1, Task.objects.count())
+        saved_instance = Task.objects.last()
+        self.assertTrue(saved_instance.id)
+        self.assertEqual('Buy beer', saved_instance.description)
+        self.assertEqual(now, saved_instance.created_at)
+        self.assertEqual(now, saved_instance.due_at)
+        self.assertEqual(now, saved_instance.done_at)
+
+    def test_str_truncates_description(self):
+        instance = Task(description='A' * 80, to_do_list=mommy.make(ToDoList))
+        self.assertEqual('%s...' % ('A' * 67), str(instance))
+
+
 
