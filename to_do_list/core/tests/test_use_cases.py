2cb6c339e97fcc180ed95913836b4e638ec80733
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-02 22:51:19 -0200

    Implement nested creation of to do list whith tasks on create to do list use case

--- to_do_list/core/tests/test_use_cases.py
+++ to_do_list/core/tests/test_use_cases.py
@@ -11,9 +11,30 @@ class CreateToDoListUseCaseTest(TestCase):
             'name': 'My empty list',
         }
         count = ToDoList.objects.count()
+
         to_do_list = create_to_do_list_use_case(data)
+
+        self.assertEqual(data['name'], to_do_list.name)
         self.assertEqual(count + 1, ToDoList.objects.count())
-        new_list = ToDoList.objects.last()
-        self.assertEqual(data['name'], new_list.name)
 
+    def test_create_to_do_list_with_tasks(self):
+        data = {
+            'name': 'My tasks list',
+            'tasks': [
+                {
+                    'description': 'Do something!'
+                }
+            ]
+        }
+        lists_count = ToDoList.objects.count()
+        tasks_count = Task.objects.count()
 
+        to_do_list = create_to_do_list_use_case(data)
+
+        self.assertEqual(lists_count + 1, ToDoList.objects.count())
+        self.assertEqual(data['name'], to_do_list.name)
+
+        self.assertEqual(tasks_count + 1, Task.objects.count())
+        self.assertEqual(1, to_do_list.tasks.count())
+        new_task = to_do_list.tasks.first()
+        self.assertEqual(data['tasks'][0]['description'], new_task.description)
--- to_do_list/core/use_cases.py
+++ to_do_list/core/use_cases.py
@@ -2,4 +2,10 @@ from core.models import Task, ToDoList
 
 
 def create_to_do_list_use_case(data):
+    data = data.copy()
+    tasks_data = data.pop('tasks', [])
     to_do_list = ToDoList.objects.create(**data)
+    for task_data in tasks_data:
+        task_data['to_do_list'] = to_do_list
+        Task.objects.create(**task_data)
+    return to_do_list
