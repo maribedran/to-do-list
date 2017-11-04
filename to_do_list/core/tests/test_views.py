a9ccfc5cefdc161cacff4392a8e50aaaf9f5f7bd
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-04 13:55:19 -0200

    Config to-do list view test to use generic CRUD test case

--- to_do_list/core/tests/test_views.py
+++ to_do_list/core/tests/test_views.py
@@ -1,81 +1,19 @@
-import json
 from django.test import TestCase
-from django.urls import reverse
-from model_mommy import mommy
 
 from core.models import Task, ToDoList
 from core.serializers import TaskSerializer, ToDoListSerializer
-from helpers.test import LoginRequiredTestCaseMixin
+from helpers.test import LoginRequiredTestCaseMixin, CRUDTestCaseMixin
 
 
-class ToDoListViewSetTest(TestCase, LoginRequiredTestCaseMixin):
+class ToDoListViewSetTest(TestCase, LoginRequiredTestCaseMixin, CRUDTestCaseMixin):
+    model_class = ToDoList
+    serializer_class = ToDoListSerializer
+    url_base_name = 'to_do_list'
+    post_data = {
+        'name': 'My new life project',
+        'tasks': []
+    }
 
     def setUp(self):
         self.set_up_client()
-
-        self.to_do_list = mommy.make(ToDoList)
-        self.list_url = reverse('to_do_list-list')
-        self.detail_url = reverse(
-            'to_do_list-detail',
-            args=[self.to_do_list.id]
-        )
-        self.data = ToDoListSerializer(instance=self.to_do_list).data
-        self.post_data = {
-            'name': 'My new life project',
-            'tasks': []
-        }
-
-    def test_list_returns_200(self):
-        response = self.client.get(self.list_url)
-        self.assertEqual(200, response.status_code)
-
-    def test_list_returns_correct_content(self):
-        response = self.client.get(self.list_url)
-        self.assertEqual([self.data], response.json())
-
-    def test_detail_returns_200(self):
-        response = self.client.get(self.detail_url)
-        self.assertEqual(200, response.status_code)
-
-    def test_detail_returns_correct_content(self):
-        response = self.client.get(self.detail_url)
-        self.assertEqual(self.data, response.json())
-
-    def test_delete_returns_204(self):
-        response = self.client.delete(self.detail_url)
-        self.assertEqual(204, response.status_code)
-
-    def test_delete_removes_entry_from_db(self):
-        count = ToDoList.objects.count()
-        self.client.delete(self.detail_url)
-        self.assertEqual(count - 1, ToDoList.objects.count())
-
-    def test_create_returns_201(self):
-        response = self.client.post(self.list_url, self.post_data)
-        self.assertEqual(201, response.status_code)
-
-    def test_create_returns_new_instance_data(self):
-        count = ToDoList.objects.count()
-        response = self.client.post(self.list_url, self.post_data)
-        self.assertEqual(count + 1, ToDoList.objects.count())
-        new_instance = ToDoList.objects.last()
-        expected_data = ToDoListSerializer(instance=new_instance).data
-        self.assertEqual(expected_data, response.json())
-
-    def test_update_returns_200(self):
-        response = self.client.put(
-            self.detail_url,
-            json.dumps(self.post_data),
-            content_type='application/json'
-        )
-        self.assertEqual(200, response.status_code)
-
-    def test_update_returns_updated_instance_data(self):
-        response = self.client.put(
-            self.detail_url,
-            json.dumps(self.post_data),
-            content_type='application/json'
-        )
-        updated_instance = ToDoList.objects.get(id=self.to_do_list.id)
-        expected_data = ToDoListSerializer(instance=updated_instance).data
-        self.assertEqual(expected_data, response.json())
+        self.set_up_objects()
