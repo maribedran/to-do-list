39db2f61a8028f9000f51101aeca66d91404d80e
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-07 01:43:18 -0200

    Add password reset confirm url using rest auth's view

--- to_do_list/to_do_list/urls.py
+++ to_do_list/to_do_list/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     url('^', include('core.urls', namespace='core')),
@@ -7,4 +8,7 @@ urlpatterns = [
     url(r'^rest-auth/', include('rest_auth.urls', namespace='rest_auth')),
     url(r'^rest-auth/registration/',
         include('rest_auth.registration.urls', namespace='registration')),
+    url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
 ]
