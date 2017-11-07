7703c4a149a9784dea4c3b3dea4ea6f214b608c7
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-07 01:41:04 -0200

    Add smtp configuration to settings

--- to_do_list/to_do_list/settings.py
+++ to_do_list/to_do_list/settings.py
@@ -135,3 +135,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+# SMTP
+
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'example@gmail.com'
+SERVER_EMAIL = 'example@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
