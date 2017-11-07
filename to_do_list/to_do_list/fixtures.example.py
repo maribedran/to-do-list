2a7eab530ba4aa05b67f9861cfc6dd3a776223fd
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-07 17:22:10 -0200

    Add fixture with two local default sites for local enviroments

---
+++ to_do_list/to_do_list/fixtures.example.py
@@ -0,0 +1,18 @@
+[
+    {
+        "model": "sites.site",
+        "pk": 1,
+        "fields": {
+            "domain": "localhost:8000",
+            "name": "Local"
+        }
+    },
+{
+    "model": "myapp.person",
+    "pk": 2,
+    "fields": {
+        "domain": "localhost:8080",
+        "name": "Local App"
+    }
+}
+]
