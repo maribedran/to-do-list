e6454fc3cb004a003122029804406571a1c32d2b
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-07 01:46:28 -0200

    Create template for email recovery

--- to_do_list/to_do_list/settings.py
+++ to_do_list/to_do_list/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'to_do_list.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'core/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
