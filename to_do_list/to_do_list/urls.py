c8616453228d76047694cc1bc7d41a64a0f6ecd7
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-11 15:01:09 -0200

    Serve frontend on root url and move core urls to /api route

--- to_do_list/to_do_list/urls.py
+++ to_do_list/to_do_list/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 from rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
-    url('^', include('core.urls', namespace='core')),
+    url('^$', TemplateView.as_view(template_name='index.html')),
+    url('^api/', include('core.urls', namespace='core')),
     url(r'^admin/', admin.site.urls),
     url(r'^rest-auth/', include('rest_auth.urls', namespace='rest_auth')),
     url(r'^rest-auth/registration/',
