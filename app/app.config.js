4640e07fcdac86054aba6b70079794def4f00fe8
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-13 00:43:41 -0200

    Remove detail and default routes from angular app config

--- app/app.config.js
+++ app/app.config.js
@@ -18,10 +18,6 @@ angular.
       $routeProvider.
         when('/to-do-lists', {
           template: '<to-do-list-list></to-do-list-list>'
-        }).
-        when('/to-do-lists/:toDoListId', {
-          template: '<to-do-list-detail></to-do-list-detail>'
-        }).
-        otherwise('/to-do-lists');
+        })
     }
   ]);
