7d38d2c0315c419318f6c8aa3f18dfc682985b29
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-12 23:50:46 -0200

    Add example settings for developement

---
+++ to_do_list/to_do_list/settings_dev.example.py
@@ -0,0 +1,13 @@
+# Django Sites Framework
+
+SITE_ID = 2
+
+# SMTP
+
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'example@gmail.com'
+SERVER_EMAIL = 'example@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
