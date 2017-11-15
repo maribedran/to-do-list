c892cf2781def1d2d3f527e09da760359b3080a9
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-14 22:49:38 -0200

    Set postgres as default database backend

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'to_do_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'to_do_list',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
